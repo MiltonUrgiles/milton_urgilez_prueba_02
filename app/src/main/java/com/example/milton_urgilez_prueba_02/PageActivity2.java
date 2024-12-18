75ed0a70b829dc48a70544c54ec76adbba5fdb73
Milton Urgiles <jolu20x@gmail.com>
2024-12-18 18:02:34 -0500

    Urgilez Milton: 18H00

--- app/src/main/java/com/example/milton_urgilez_prueba_02/PageActivity2.java
+++ app/src/main/java/com/example/milton_urgilez_prueba_02/PageActivity2.java
@@ -1,7 +1,6 @@
 package com.example.milton_urgilez_prueba_02;
 
 import android.os.Bundle;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,23 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-
-    EditText nombres;
-    EditText apellidos;
-    EditText dividendos;
-    EditText nombres;
-    EditText nombres;
-    EditText nombres;
-    EditText nombres;
-
+public class PageActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_page2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
